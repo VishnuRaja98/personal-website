daafe6492e0e3cf551284335c4f19a9b4ad44357
VishnuRaja98 <vishnu1234raja@gmail.com>
2022-11-09 22:23:10 +0530

    Revert "Merge branch 'master' of https://github.com/VishnuRaja98/personal-website"
    This reverts commit ea53fdf802f46fc06a74ee368f587cdb90bfd298.

---
+++ src/pages/emailverified.js
@@ -0,0 +1,12 @@
+import React from 'react';
+  
+const EmailVerified = () => {
+  return (
+    <div class="App-header">
+      <h1>Email Verified!</h1>
+      <h2>You can close this page</h2>
+    </div>
+  );
+};
+  
+export default EmailVerified;
